d3d17a6bb1afaad5f280a97a74c936007b57e6cb
wuxianqiang <2631640352@qq.com>
2021-01-20 11:18:19 +0800

    vue2版本title滚动

---
+++ vue2-demo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <scroll-title :list="list" :activeTitleStyle="{color: 'red'}">
+    </scroll-title>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ScrollTitle from './components/ScrollTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ScrollTitle
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.list = [
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+        '标题1',
+      ]
+    }, 2000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
